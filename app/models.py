28477b59b17144a1dc288a6ab72cef80df0bd4ab
Ali <alicandonmez90@gmail.com>
2024-01-31 14:18:54 +0100

    reorganize app structure and add sqlalchemy models

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    hashed_password = Column(String)
+    deposit = Column(Integer)
+    role = Column(String)
+
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    amount_available = Column(Integer)
+    cost = Column(Integer)
+    product_name = Column(String)
+    seller_id = Column(Integer, ForeignKey('user.id'))
+
+    seller = relationship('User', back_populates='product')
--- app/schemas.py
+++ app/schemas.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel
 from pydantic import Field
 
 
+class User(BaseModel):
+    username: str = Field(examples=['TylerDurden'])
+    password: str = Field(examples=['Az195#?'])
+    deposit: int = Field(examples=[50])
+    role: str = Field(examples=['seller', 'buyer'])  # TODO: Make another Model or Enum
+
+
 class Product(BaseModel):
     amount_available: int = Field(examples=[5])
     cost: int = Field(examples=[5, 10, 15, 20, 50, 100])
     product_name: str = Field(examples=['Cola'])
     seller_id: int = Field(examples=[2])
-
-
-class User(BaseModel):
-    username: str = Field(examples=['TylerDurden'])
-    password: str = Field(examples=['Az195#?'])
-    deposit: int = Field(examples=[50])
-    role: str = Field(examples=['seller', 'buyer'])  # make another Model or Enum
