ba5ff5e8b041b392b066b49b70d5bfafc5e4118c
Ali <alicandonmez90@gmail.com>
2024-01-31 18:55:45 +0100

    implement crud for product

--- app/crud.py
+++ app/crud.py
@@ -47,3 +47,44 @@ def delete_user(db: Session, user_id: int):
     db_user = get_user(db, user_id)
     db.delete(db_user)
     db.commit()
+
+
+# Product
+def get_product(db: Session, user_id: int):
+    return db.query(models.Product).filter(models.Product.id == user_id).first()
+
+
+def get_products(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Product).offset(skip).limit(limit).all()
+
+
+def create_product(db: Session, product: schemas.Product):
+    db_product = models.Product(
+        amount_available=product.amount_available,
+        cost=product.cost,
+        product_name=product.product_name,
+        seller_id=product.seller_id,
+    )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+def update_product(db: Session, product_id: int, user: schemas.Product):
+    db_product = get_product(db, product_id)
+
+    db_product.amount_available = user.amount_available
+    db_product.cost = user.cost
+    db_product.product_name = user.product_name
+    db_product.seller_id = user.seller_id
+
+    db.add(db_product)
+    db.commit()
+    return db_product
+
+
+def delete_product(db: Session, product_id: int):
+    db_product = get_product(db, product_id)
+    db.delete(db_product)
+    db.commit()
