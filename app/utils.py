6d4b577cc951b1bb4726deb4cefe432eaa5eb0f8
Ali <alicandonmez90@gmail.com>
2024-01-31 20:29:54 +0100

    fix hash_password

--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,6 @@ def hash_password(plain_password):
     Hash a password with a different salt everytime.
     """
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(plain_password, salt)
+    hashed = bcrypt.hashpw(str.encode(plain_password), salt)
 
     return hashed
