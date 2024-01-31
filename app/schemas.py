42d8a6cb71680627dfd7652c15dd5d3eac85b67e
Ali <alicandonmez90@gmail.com>
2024-01-31 17:32:47 +0100

    add Pydantic schemas

--- app/schemas.py
+++ app/schemas.py
@@ -2,15 +2,40 @@ from pydantic import BaseModel
 from pydantic import Field
 
 
-class User(BaseModel):
+class UserBase(BaseModel):
+    """
+    Base class for User
+    """
     username: str = Field(examples=['TylerDurden'])
-    password: str = Field(examples=['Az195#?'])
     deposit: int = Field(examples=[50])
     role: str = Field(examples=['seller', 'buyer'])  # TODO: Make another Model or Enum
 
 
+class UserCreate(UserBase):
+    """
+    For creating
+    """
+    password: str = Field(examples=['Az195#?'])
+
+
+class User(UserBase):
+    """
+    For reading and returning
+    """
+    pass
+
+    class Config:
+        orm_mode = True
+
+
 class Product(BaseModel):
+    """
+    Product schema for reading, returning and creating
+    """
     amount_available: int = Field(examples=[5])
     cost: int = Field(examples=[5, 10, 15, 20, 50, 100])
     product_name: str = Field(examples=['Cola'])
     seller_id: int = Field(examples=[2])
+
+    class Config:
+        orm_mode = True
