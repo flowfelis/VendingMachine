13fa23a63a2736080f05e3f484216b10a9ddcf6c
Ali <alicandonmez90@gmail.com>
2024-01-31 20:31:09 +0100

    add request handlers for user crud

--- app/main.py
+++ app/main.py
@@ -1,13 +1,66 @@
-from fastapi import FastAPI
+from typing import List
 
+from fastapi import Depends
+from fastapi import FastAPI
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from app import crud
+from app import models
+from app import schemas
+from app.database import SessionLocal
+from app.database import engine
+
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/hello/{name}")
-def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# User
+@app.get('/users/', response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get('/users/{user_id}', response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post('/users/', response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail='User already exists')
+    return crud.create_user(db=db, user=user)
+
+
+@app.put('/users/{user_id}', response_model=schemas.User)
+def update_user(user_id: int, user: schemas.User, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.update_user(db, user_id, user)
+
+
+@app.delete('/users/{user_id}', response_model=schemas.User)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(db_user)
+    db.commit()
+    return db_user
